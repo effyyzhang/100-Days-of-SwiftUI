8e6830143c237e52f8db1cb584aef78b54b906bb
Effy Zhang <effyyzhang@gmail.com>
2020-11-27 19:21:04 -0800

    wrap up project 6

---
+++ Project 6/Project 6/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project 6
+//
+//  Created by Effy Zhang on 11/26/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project 6/Project 6/Project_6App.swift
@@ -0,0 +1,17 @@
+//
+//  Project_6App.swift
+//  Project 6
+//
+//  Created by Effy Zhang on 11/26/20.
+//
+
+import SwiftUI
+
+@main
+struct Project_6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
