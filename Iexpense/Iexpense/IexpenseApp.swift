422e345186479f85f8b10b3bc9dddbe34457c5a5
Effy Zhang <effyyzhang@gmail.com>
2021-01-07 12:42:58 -0800

    Add project 7: Iexpense

---
+++ Iexpense/Iexpense/IexpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  IexpenseApp.swift
+//  Iexpense
+//
+//  Created by Effy Zhang on 1/6/21.
+//
+
+import SwiftUI
+
+@main
+struct IexpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
